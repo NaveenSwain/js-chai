5163e0b16584b26ccadfbf1fd4a2496d07b2375a
NaveenSwain <naveenswain06@gmail.com>
2025-11-26 11:18:23 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+//values
+//"33"=>33
+//"33abc"=>NaN
+//true=>1;false=>0
+
+let isloggedin =1
+let booleanIsLoggedIn = Boolean(isloggedin)
+console.log(booleanIsLoggedIn);
+
+//1 =>true; 0=>false
+//"" =>false
+//"naveen" => true
+
+let someNumber =78
+let stringNum=String(someNumber)
+console.log(stringNum);
+console.log(typeof stringNum);
