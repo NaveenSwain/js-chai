b880162154e89838cf62d440842dd3eeab0abfda
NaveenSwain <naveenswain06@gmail.com>
2025-11-26 10:57:40 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as new version
+// alert(3+3)//we are using nodejs not browser
+
+console.log(3+3)
+console.log("naveen");
+
+let name="naveen"
+let age=20
+let isloggedin=false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
